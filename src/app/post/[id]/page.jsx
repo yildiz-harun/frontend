ad98f0a119b396f26e574c8baa23f550c545a58e
Harun <harun.yildiz@oroox.com>
2023-09-01 10:05:55 +0300

    Add

--- src/app/post/[id]/page.jsx
+++ src/app/post/[id]/page.jsx
@@ -1,5 +1,18 @@
-import React from "react";
+"use client"
+import { useEffect, useState } from "react";
+import PostDetail from "@/app/components/PostDetail";
 
-export default function Post({ params }) {
-  return <p>{params.id}</p>;
+export default function Post({params}) {
+  const [post, setPost] = useState(null);
+  useEffect(() => {
+    if (params.id) {
+      fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
+        .then((res) => res.json())
+        .then((data) => setPost(data));
+    }
+  }, [params.id]);
+
+  if (!post) return <div>Loading...</div>;
+
+  return <PostDetail id={params.id} title={post.title} content={post.body} />;
 }
