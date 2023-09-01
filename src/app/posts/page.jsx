5aad86d00c7631cfbaeb9478e5b3a6807172255f
Harun <harun.yildiz@oroox.com>
2023-09-01 14:02:59 +0300

    Add: create new post button

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchInitialPosts } from "../GlobalRedux/Features/Posts/postsSlice";
 import { setFetched } from "../GlobalRedux/Features/Posts/isFetchedSlice";
-
+import Link from "next/link";
 import PostCard from "../components/PostCard";
 
 export default function Posts() {
@@ -19,20 +19,23 @@ export default function Posts() {
   }, [dispatch]);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 w-full bg-white ">
-      {posts
-        .filter((post) => post.userId === 1)
-        .map((post, index, array) => (
-          <PostCard
-            className={`${
-              index === array.length - 1 ? "justify-self-start" : ""
-            }`}
-            key={post.id}
-            id={post.id}
-            title={post.title}
-            content={post.body}
-          />
-        ))}
-    </div>
+    <>
+      <Link className="bg-green-500 block w-fit mb-6  text-white rounded-lg p-2" href={"/newPost"}>Create new post</Link>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 w-full bg-white ">
+        {posts
+          .filter((post) => post.userId === 1)
+          .map((post, index, array) => (
+            <PostCard
+              className={`${
+                index === array.length - 1 ? "justify-self-start" : ""
+              }`}
+              key={post.id}
+              id={post.id}
+              title={post.title}
+              content={post.body}
+            />
+          ))}
+      </div>
+    </>
   );
 }
